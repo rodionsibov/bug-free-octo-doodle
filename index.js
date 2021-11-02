4267f314e85a9e671ea1805647ec7bd3777b04cb
Rodion Sibov <rodionsibov@gmail.com>
2021-11-02 16:01:37 +0200

    get articles as json

--- index.js
+++ index.js
@@ -4,12 +4,14 @@ const cheerio = require('cheerio')
 const express = require('express')
 
 const app = express()
+
 const url = 'https://www.theguardian.com/uk'
+const articles = []
+
 axios.get(url)
     .then(res => {
         const html = res.data
         const $ = cheerio.load(html)
-        const articles = []
 
         $('.fc-item__title', html).each(function () {
             const title = $(this).text()
@@ -19,7 +21,10 @@ axios.get(url)
                 url
             })
         })
-        console.log(articles);
     }).catch(err => console.log(err))
+
+app.get('/', (req, res) => {
+    res.json(articles)
+})
 
 app.listen(PORT, () => console.log(`Server running on PORT ${PORT}`))
