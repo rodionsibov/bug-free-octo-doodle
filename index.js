bf7fe74c52bdb66ce332e2044631f879a1adfaa6
Rodion Sibov <rodionsibov@gmail.com>
2021-11-02 11:31:09 +0200

    update

--- index.js
+++ index.js
@@ -6,9 +6,20 @@ const express = require('express')
 const app = express()
 const url = 'https://www.theguardian.com/uk'
 axios.get(url)
-.then(res => {
-    const html = res.data
-    console.log(html);
-})
+    .then(res => {
+        const html = res.data
+        const $ = cheerio.load(html)
+        const articles = []
+
+        $('.fc-item__title', html).each(function () {
+            const title = $(this).text()
+            const url = $(this).find('a').attr('href')
+            articles.push({
+                title,
+                url
+            })
+        })
+        console.log(articles);
+    }).catch(err => console.log(err))
 
 app.listen(PORT, () => console.log(`Server running on PORT ${PORT}`))
